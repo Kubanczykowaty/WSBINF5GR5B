2dc922bbee59aa0fef98c9570db5dcd58647561c
Kubanczykowaty <jakubowczarczyk97@gmail.com>
2020-06-26 13:34:03 +0200

    Dodanie kolejnych modulow: dodawanie, odejmowanie, dzielenie, mnozenie

---
+++ dzielenie.py
@@ -0,0 +1,6 @@
+def dzielenie():
+    print("Witaj w module dzielenia!")
+    a = int(input(("Podaj pierwsza liczbe: ")))
+    b = int(input(("Podaj druga liczbe: ")))
+    wynik = a/b
+    print("Wynik dzielenia podanych liczb wynosi:", wynik)
---
+++ mnozenie.py
@@ -0,0 +1,6 @@
+def mnozenie():
+    print("Witaj w module mnozenia!")
+    a = int(input(("Podaj pierwsza liczbe: ")))
+    b = int(input(("Podaj druga liczbe: ")))
+    wynik = a*b
+    print("Wynik mnozenia podanych liczb wynosi:", wynik)
