ceddaad511217da89e8fef388f1a8658b6aeeb38
Kubanczykowaty <jakubowczarczyk97@gmail.com>
2020-06-26 15:25:49 +0200

    Dodanie menu

---
+++ menu.py
@@ -0,0 +1,3 @@
+def menu():
+    print("Witaj w prostym kalkulatorze. \nWybierz co chcesz zrobic wpisujac numer odpowiadjacy opcji menu:")
+    print("\n*************************************\n1. Dodawanie \n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n\nq aby Wyjsc\n*************************************")
