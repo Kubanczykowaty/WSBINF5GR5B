74ba0cc59c217e1c2df675896f0fb64d9bcc1a1d
Moolenda <61930897+Moolenda@users.noreply.github.com>
2020-06-28 19:36:21 +0200

    Strona logowania

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Logowanie</title>
+</head>
+<body>
+	<h1>Panel logowania</h1>
+	<input type="text" placeholder="Login">
+	<input type="password" placeholder="Haslo">
+	<input type="submit" value="Zaloguj">
+</body>
+</html>
