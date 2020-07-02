b3b2e548f43695f56bf6c3144e7a58d84018183f
WadiHalfa-P <patryk.mucha.it@gmail.com>
2020-07-02 23:09:20 +0200

    petle

---
+++ petle.py
@@ -0,0 +1,13 @@
+znak = chr("Wprowadz znak: ")
+int(i)
+int(j)
+kon = chr(A)
+ile_liter = int(1)
+znak - kon = int(ile)
+  for (i=0; i <= ile; i++)
+  pocz = znak
+  for (j=0; j < ile_liter; j++;)
+  print(%znak, pocz--)
+  ile_liter++
+  print('\n')
+return 0
